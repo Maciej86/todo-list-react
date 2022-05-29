1e61d6ce9e5c5a1d6e2f2c51353edcd12de02f29
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-05-29 20:39:07 +0200

    Changes tab size

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,34 +2,34 @@ import { useState, useEffect, useRef } from "react";
 import { StyledForm, Input, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
-	const [newTaskContent, setNewTaskContent] = useState("");
-	const inputFocus = useRef(null);
+  const [newTaskContent, setNewTaskContent] = useState("");
+  const inputFocus = useRef(null);
 
-	useEffect(() => {
-		inputFocus.current.focus();
-	}, []);
+  useEffect(() => {
+    inputFocus.current.focus();
+  }, []);
 
-	const onFormSubmit = (event) => {
-		event.preventDefault();
-		const trimNewTaskContent = newTaskContent.trim();
-		if (trimNewTaskContent !== "") {
-			addNewTask(trimNewTaskContent);
-		}
-		setNewTaskContent("");
-		inputFocus.current.focus();
-	};
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    const trimNewTaskContent = newTaskContent.trim();
+    if (trimNewTaskContent !== "") {
+      addNewTask(trimNewTaskContent);
+    }
+    setNewTaskContent("");
+    inputFocus.current.focus();
+  };
 
-	return (
-		<StyledForm onSubmit={onFormSubmit}>
-			<Input
-				value={newTaskContent}
-				onChange={({ target }) => setNewTaskContent(target.value)}
-				type="text"
-				ref={inputFocus}
-			/>
-			<Button>Dodaj zadanie</Button>
-		</StyledForm>
-	);
+  return (
+    <StyledForm onSubmit={onFormSubmit}>
+      <Input
+        value={newTaskContent}
+        onChange={({ target }) => setNewTaskContent(target.value)}
+        type="text"
+        ref={inputFocus}
+      />
+      <Button>Dodaj zadanie</Button>
+    </StyledForm>
+  );
 };
 
 export default Form;

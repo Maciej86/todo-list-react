1e9cd5abd863760361c7d6cc1307afed343c0627
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-07-11 00:07:31 +0200

    Creating subpages in the application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./common/Navigation";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
-const App = () => <Tasks />;
+const App = () => (
+  <HashRouter>
+    <Navigation />
+    <Routes>
+      <Route path="/zadania" element={<Tasks />} />
+      <Route path="/autor" element={<Author />} />
+    </Routes>
+  </HashRouter>
+);
 
 export default App;
